3f81ae35d17edc0b2c8d1d86d0a75e8cd607b71b
Andrew Rodland <andrew@cleverdomain.org>
2020-05-31 03:09:52 -0400

    Add endpoints for accessing pred, irimap, and assimilated
    * /pred.json: predicted station data
    * /irimap.h5: IRI-model predictions
    * /assimilated.h5: assimilated predictions
    
    TODO probably, an eSSN endpoint.

--- api/app/main.py
+++ api/app/main.py
@@ -1,9 +1,10 @@
-from flask import Flask, request, jsonify, render_template, send_file
+from flask import Flask, request, jsonify, render_template, send_file, make_response
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from marshmallow import Schema, fields
 import json
 import os
+import datetime as dt
 from sqlalchemy import and_, text
 
 app = Flask(__name__)
@@ -52,6 +53,20 @@ class Measurement(db.Model):
     def __repr__(self):
         return '<Measurement %r>' % self.id
 
+
+class Prediction(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    run_id = db.Column(db.Integer)
+    time = db.Column(db.DateTime)
+    cs = db.Column(db.Numeric(asdecimal=False))
+    fof2 = db.Column(db.Numeric(asdecimal=False))
+    mufd = db.Column(db.Numeric(asdecimal=False))
+    hmf2 = db.Column(db.Numeric(asdecimal=False))
+    station_id = db.Column(db.Integer, db.ForeignKey('station.id'))
+    station_name = db.relationship('Station', foreign_keys=[station_id])
+    def __repr__(self):
+        return '<Prediction %r>' % self.id
+
 #Generate marshmallow Schemas from your models using ModelSchema
 
 class StationSchema(ma.ModelSchema):
@@ -69,6 +84,14 @@ class MeasurementSchema(ma.ModelSchema):
 measurement_schema = MeasurementSchema()
 measurements_schema = MeasurementSchema(many=True)
 
+class PredictionSchema(ma.ModelSchema):
+    class Meta:
+        model = Prediction
+    station = fields.Nested('StationSchema', only=['name', 'id', 'code', 'longitude', 'latitude'])
+
+prediction_schema = PredictionSchema()
+predictionss_schema = PredictionSchema(many=True)
+
 #You can now use your schema to dump and load your ORM objects.
 
 #Returns latest measurements for all stations in JSON
@@ -84,6 +107,54 @@ def stationsjson():
 
     return jsonify(result.data)
 
+@app.route("/pred.json" , methods=['GET'])
+def predjson():
+    run_id = request.args.get('run_id', None)
+    ts = dt.datetime.fromtimestamp(float(request.args.get('ts', None)))
+
+    qry = db.session.query(Measurement).from_statement(
+            "select p.* from prediction p where run_id=:run_id and time=:ts order by station_id asc").params(
+                    run_id = run_id,
+                    ts = ts,
+            )
+    db.session.close()
+    
+    result = predictionss_schema.dump(qry)
+
+    return jsonify(result.data)
+
+@app.route("/irimap.h5", methods=['GET'])
+def irimap():
+    run_id = request.args.get('run_id', None)
+    ts = dt.datetime.fromtimestamp(float(request.args.get('ts', None)))
+
+    with db.engine.connect() as conn:
+        res = conn.execute("select dataset from irimap where run_id=%s and time=%s",
+            (run_id, ts),
+        )
+        rows = list(res.fetchall())
+
+        if len(rows) == 0:
+            return make_response('Not Found', 404)
+
+        return make_response(rows[0]['dataset'].tobytes(), { 'Content-Type': 'application/x-hdf5' })
+        
+@app.route("/assimilated.h5", methods=['GET'])
+def assimilated():
+    run_id = request.args.get('run_id', None)
+    ts = dt.datetime.fromtimestamp(float(request.args.get('ts', None)))
+
+    with db.engine.connect() as conn:
+        res = conn.execute("select dataset from assimilated where run_id=%s and time=%s",
+            (run_id, ts),
+        )
+        rows = list(res.fetchall())
+
+        if len(rows) == 0:
+            return make_response('Not Found', 404)
+
+        return make_response(rows[0]['dataset'].tobytes(), { 'Content-Type': 'application/x-hdf5' })
+        
 @app.route("/", methods=['GET'])
 def static_stations():
       index_path = os.path.join(app.static_folder, 'index.html')
@@ -93,4 +164,4 @@ def static_stations():
                             #latestmes = latestmes
 
 if __name__ == "__main__":
-    app.run(debug=False,host='0.0.0.0', port=80)
+    app.run(debug=False,host='0.0.0.0', port=80, threaded=False, processes=4)
