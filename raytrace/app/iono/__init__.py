c66ce16543aabb5c941928954bd62b48420da2bd
Andrew Rodland <andrew@cleverdomain.org>
2020-12-04 01:31:18 -0500

    raytrace: replace interpolator
    use a numba-compiled bilinear interpolator for the ionospheric
    parameters instead of scipy.interpolate.RectBivariateSpline.
    
    It's faster and the precision loss is tiny.
    
    I tried doing the same thing for assimilate: it works, but the
    interpolation is a much smaller portion of the work there, so it doesn't
    make any significant difference.

--- raytrace/app/iono/__init__.py
+++ raytrace/app/iono/__init__.py
@@ -1,19 +1,38 @@
 import numpy as np
-import scipy.interpolate
+from numba import jit
 import h5py
 import urllib.request
 import io
+import math
+
+@jit('f8[:](f8[:,:],f8[:],f8[:])',nopython=True,error_model='numpy')
+def bilerp(table, latitude, longitude):
+    lat = latitude * 180 / np.pi + 90
+    lon = longitude * 180 / np.pi + 180
+    out = np.zeros_like(lat)
+
+    for i in range(lat.size):
+        latc = math.ceil(lat[i])
+        lonf = math.floor(lon[i])
+        lonc = math.ceil(lon[i])
+        latf, latc = int(np.floor(lat[i])), int(np.ceil(lat[i]))
+        latp = lat[i] - latf
+        lonf, lonc = int(np.floor(lon[i])), int(np.ceil(lon[i]))
+        lonp = lon[i] - lonf
+
+        top = lonp * table[latc, lonc] + (1.-lonp) * table[latc, lonf]
+        bot = lonp * table[latf, lonc] + (1.-lonp) * table[latf, lonf]
+
+        out[i] = latp * top + (1.-latp) * bot
+
+    return out
 
 class Spline:
     def __init__(self, table):
-        self.table = table
-        lat = np.linspace(-np.pi / 2, np.pi / 2, 181)
-        lon = np.linspace(-np.pi, np.pi, 361)
-
-        self.spline = scipy.interpolate.RectBivariateSpline(lat, lon, self.table)
+        self.table = np.array(table)
 
     def predict(self, latitude, longitude):
-        return self.spline(latitude.flatten(), longitude.flatten(), grid=False).reshape(longitude.shape)
+        return bilerp(self.table, latitude.flatten(), longitude.flatten()).reshape(longitude.shape)
 
 class Iono:
     def __init__(self, url):
