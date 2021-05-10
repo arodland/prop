97232dbdd602d0d736ae750eeb307f0e4c87b23d
Andrew Rodland <andrew@cleverdomain.org>
2021-05-10 13:56:42 -0400

    storm: switch to a new data file from GFZ Potsdam
    the old one stopped updating, and this one is easier to use anyway

--- storm/app/main.py
+++ storm/app/main.py
@@ -5,13 +5,6 @@ import io
 import os
 import shutil
 
-def mldt(ml):
-    dt = datetime.datetime(year=1,month=1,day=1) + datetime.timedelta(days=ml) - datetime.timedelta(days=366) 
-    # round to nearest minute
-    dt += datetime.timedelta(seconds=30)
-    dt -= datetime.timedelta(seconds=dt.second, microseconds=dt.microsecond)
-    return dt
-
 def kp_to_ap(kp):
     idx = int((kp + 0.1) * 3)
     return [0,2,3,4,5,6,7,9,12,15,18,22,27,32,39,48,56,67,80,94,111,132,154,179,207,236,300,400][idx]
@@ -24,16 +17,13 @@ with open('/iri-index/predicted/apf107.tmp', 'w') as out:
             nlines += 1
 
     data = {}
-    with urllib.request.urlopen("https://spaceweather.gfz-potsdam.de/fileadmin/michaeli/rbm_forecast_v2/latest/Forecast_E_1_MeV_PA_50_UTC_latest.mat") as res:
+    with urllib.request.urlopen("https://spaceweather.gfz-potsdam.de/fileadmin/ruggero/Kp_forecast/forecast_figures/KP_FORECAST_CURRENT.mat") as res:
         content = res.read()
         bio = io.BytesIO(content)
         mat = scipy.io.loadmat(bio)
 
-        for time, kp in zip(mat['times'][0], mat['Kp'][0]):
-            dt = mldt(time)
-            dt += datetime.timedelta(seconds=30)
-            dt -= datetime.timedelta(seconds=dt.second, microseconds=dt.microsecond)
-
+        for time, kp in zip(mat['t'], mat['kp'][0]):
+            dt = datetime.datetime.strptime(time, '%Y-%m-%d %H:%M:%S')
             data[dt] = kp_to_ap(kp)
 
     keys_sorted = sorted(data.keys())
@@ -49,18 +39,14 @@ with open('/iri-index/predicted/apf107.tmp', 'w') as out:
         ap_day = 0
 
         if day >= first_day:
-            hourly = []
-            for hour in range(24):
-                dt = datetime.datetime(year=day.year, month=day.month, day=day.day, hour=hour)
-                if dt < first:
-                    hourly.append(data[first])
-                elif dt > last:
-                    hourly.append(data[last])
-                else:
-                    hourly.append(data[dt])
-
             for idx in range(8):
-                ap[idx] = (hourly[3*idx] + hourly[3*idx + 1] + hourly[3*idx + 2])/3
+                dt = datetime.datetime(year=day.year, month=day.month, day=day.day, hour=idx*3)
+                if dt < first:
+                    ap[idx] = data[first]
+                elif dt > last:
+                    ap[idx] = data[last]
+                else:
+                    ap[idx] = data[dt]
 
             ap_day = round(sum(ap) / len(ap))
             ap = [round(a) for a in ap]
