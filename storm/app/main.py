7fd8184cef20f3b22d625fd0bf8210259c7017b0
Andrew Rodland <andrew@cleverdomain.org>
2021-05-28 22:20:48 -0400

    storm: combine GFZ quicklook with forecast
    the previous forecast I was using included some recent past, but the
    current one starts at 0000Z of the current day, which is generally after
    the end of the CHAIM data. Fill in the gap with the GFZ quicklook file.

--- storm/app/main.py
+++ storm/app/main.py
@@ -16,7 +16,9 @@ with open('/iri-index/predicted/apf107.tmp', 'w') as out:
             print(line, file=out, end='')
             nlines += 1
 
-    data = {}
+    forecast = {}
+    ql = {}
+
     with urllib.request.urlopen("https://spaceweather.gfz-potsdam.de/fileadmin/ruggero/Kp_forecast/forecast_figures/KP_FORECAST_CURRENT.mat") as res:
         content = res.read()
         bio = io.BytesIO(content)
@@ -24,29 +26,60 @@ with open('/iri-index/predicted/apf107.tmp', 'w') as out:
 
         for time, kp in zip(mat['t'], mat['kp'][0]):
             dt = datetime.datetime.strptime(time, '%Y-%m-%d %H:%M:%S')
-            data[dt] = kp_to_ap(kp)
+            forecast[dt] = kp_to_ap(kp)
 
-    keys_sorted = sorted(data.keys())
-    first = keys_sorted[0]
-    last = keys_sorted[-1]
-    first_day = datetime.date(year=first.year, month=first.month, day=first.day)
-    last_day = datetime.date(year=last.year, month=last.month, day=last.day)
+    with urllib.request.urlopen("http://www-app3.gfz-potsdam.de/kp_index/Kp_ap_nowcast.txt") as res:
+        # alt: ftp://ftp.gfz-potsdam.de/pub/home/obs/Kp_ap_Ap_SN_F107/Kp_ap_nowcast.txt
+        for line in res:
+            line = line.decode('utf-8')
+            if line.startswith("#"):
+                continue
+            fields = line.split()
+            dt = datetime.datetime(
+                year = int(fields[0]),
+                month = int(fields[1]),
+                day = int(fields[2]),
+                hour = int(float(fields[3])),
+                minute = 0,
+                second = 0,
+            )
+            ap = int(fields[8])
+            if ap == -1:
+                continue
+
+            ql[dt] = ap
+
+    keys_sorted = sorted(forecast.keys())
+    fc_first = keys_sorted[0]
+    fc_last = keys_sorted[-1]
+    fc_first_day = datetime.date(year=fc_first.year, month=fc_first.month, day=fc_first.day)
+    fc_last_day = datetime.date(year=fc_last.year, month=fc_last.month, day=fc_last.day)
+
+    keys_sorted = sorted(ql.keys())
+    ql_first = keys_sorted[0]
+    ql_last = keys_sorted[-1]
+    ql_first_day = datetime.date(year=ql_first.year, month=ql_first.month, day=ql_first.day)
+    ql_last_day = datetime.date(year=ql_last.year, month=ql_last.month, day=ql_last.day)
 
     day = datetime.date(1958,1,1) + datetime.timedelta(days=nlines)
 
-    while day <= last_day:
+    while day <= fc_last_day:
         ap = [0,0,0,0,0,0,0,0]
         ap_day = 0
 
-        if day >= first_day:
+        if day >= ql_first_day:
             for idx in range(8):
                 dt = datetime.datetime(year=day.year, month=day.month, day=day.day, hour=idx*3)
-                if dt < first:
-                    ap[idx] = data[first]
-                elif dt > last:
-                    ap[idx] = data[last]
+                if dt < ql_first:
+                    ap[idx] = ql[ql_first]
+                elif dt <= ql_last:
+                    ap[idx] = ql[dt]
+                elif dt < fc_first:
+                    ap[idx] = forecast[fc_first]
+                elif dt <= fc_last:
+                    ap[idx] = forecast[dt]
                 else:
-                    ap[idx] = data[dt]
+                    ap[idx] = forecast[fc_last]
 
             ap_day = round(sum(ap) / len(ap))
             ap = [round(a) for a in ap]
